8d430095846d3c7289b37a72de63e6051d683606
senchak100gmail.com <senchak100@gmail.com>
2026-04-26 00:46:08 +0300

    API key

--- app.py
+++ app.py
@@ -3273,7 +3273,7 @@ def ai_page():
     return render_template("AI.html")
 
 # Налаштування Gemini (можна винести за межі маршруту)
-api_key = os.getenv("GOOGLE_API_KEY")
+api_key = os.getenv("Gemini API Key")
 genai.configure(api_key=api_key)
 
 # Вибираємо модель (flash — найшвидша і безкоштовна)
